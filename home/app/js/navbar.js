069c7283e5b95c61e1dec7ee0d08d2ce9297704c
Karnik Kanojia <72961346+karnikkanojia@users.noreply.github.com>
2022-02-01 23:21:01 +0530

    index.html navbar properly sticky

--- home/app/js/navbar.js
+++ home/app/js/navbar.js
@@ -1,6 +1,9 @@
+const vw = Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0)
+const vh = Math.max(document.documentElement.clientHeight || 0, window.innerHeight || 0)
+
 document.addEventListener("DOMContentLoaded", function () {
   window.addEventListener("scroll", function () {
-    if (window.scrollY > 700) {
+    if (window.scrollY > vh) {
       document.getElementById("navbar_top").classList.add("fixed-top");
       // add padding top to show content behind navbar
       navbar_height = document.querySelector(".navbar").offsetHeight;

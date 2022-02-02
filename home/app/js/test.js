9d46350ac44da407bdf6fb4b11b4c127da68c5b7
anshuman <anshuman.phadke@gmail.com>
2022-02-02 17:55:10 +0530

    Website Final

--- home/app/js/test.js
+++ home/app/js/test.js
@@ -7,9 +7,14 @@ jQuery(document).ready(function($) {
                 items: 3,
                 margin: 0,
                 autoplay: true,
-                dots:true,
+                dots:false,
                 autoplayTimeout: 8500,
                 smartSpeed: 200,
+              nav: true,
+                navText: [
+        '<i class="fa fa-angle-left" id="fa" aria-hidden="true"></i>',
+        '<i class="fa fa-angle-right" id="fa" aria-hidden="true"></i>'
+    ],
                 responsive: {
                   0: {
                     items: 1
